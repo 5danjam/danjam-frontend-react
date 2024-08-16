6ab75a2529bb5e85b55c79c15fa23b7da1db7592
hwijaejeong <gnltps18@naver.com>
2024-08-16 15:20:54 +0900

    test5

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ function Main() {
     return (
         <>
             <div>
-                <h1>Tlvkf main1 18</h1>
+                <h1>Tlvkf main 18</h1>
             </div>
             {/*<Search/>*/}
         </>
